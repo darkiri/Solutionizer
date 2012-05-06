2d603d5ed64a0c95c4ca07aa55e0358db8c79c19
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-05-06 20:36:51 +0200

    MSI package creation

--- default.ps1
+++ default.ps1
@@ -1,9 +1,11 @@
 Framework "4.0"
 
+. .\psake_ext.ps1
+
 Properties {
-    $build_dir = Split-Path $psake.build_script_file
-    $build_artifacts_dir = "$build_dir\build\"
-    $solution_file = "$build_dir\solutionizer.sln"
+    $base_dir = Split-Path $psake.build_script_file
+    $build_artifacts_dir = "$base_dir\build"
+    $solution_file = "$base_dir\solutionizer.sln"
 }
 
 task Default -depends Clean, Compile
@@ -31,7 +33,7 @@ task CreateAssemblyInfo {
 [assembly: AssemblyInformationalVersion(""$majorVersion.$minorVersion.$patchVersion.$revisionCount-$commitVersion"")]
 [assembly: AssemblyFileVersion(""$majorVersion.$minorVersion.$patchVersion.$revisionCount"")]"
 
-    $file = Join-Path $build_dir "CommonAssemblyInfo.cs"
+    $file = Join-Path $base_dir "CommonAssemblyInfo.cs"
     Write-Host "Generating assembly info file $file"
     Write-Output $asmInfo > $file
 }
@@ -51,4 +53,35 @@ Task Clean {
     
     Write-Host "Cleaning $solution_file" -ForegroundColor Green
     Exec { msbuild $solution_file /t:Clean /p:Configuration=Release /v:minimal } 
+}
+
+Task Package {
+    Write-Host "Creating package" -ForegroundColor Green   
+
+    $wixUrl = "http://wixtoolset.org/releases/v3.6.2823.0/wix36-binaries.zip"
+
+    $wixZipFile = "$base_dir\packages\wix.zip"
+    $wix_dir = "$base_dir\packages\wix-3.6"
+    $candle_path = "$wix_dir\candle.exe"
+    $light_path = "$wix_dir\light.exe"
+    $heat_path = "$wix_dir\heat.exe"
+
+    if (!(Test-Path $wix_dir)) {
+        if (!(Test-Path $wixZipFile)) {
+            Write-Host "Downloading WiX toolset from $wixUrl"
+            Get-WebFile -url $wixUrl -fileName $wixZipFile
+        }
+
+        New-Item $wix_dir -Type directory | Out-Null
+        Write-Host "Extracting WiX toolset to $wix_dir"
+        Expand-ZipFile -zipPath $wixZipFile -destination $wix_dir
+    }
+
+    #& "$heat_path" dir "$build_artifacts_dir" -cg SolutionizerFiles -gg -scom -sreg -sfrag -srd -dr INSTALLLOCATION -var env.SolutionizerFiles -out "$build_artifacts_dir\FilesFragment.wxs"
+
+    #Exec { &"$candle_path" .\Solutionizer.wxs -dARTIFACTSDIR=$build_artifacts_dir -out ""$build_artifacts_dir\\"" }
+    Exec { &"$candle_path" .\Solutionizer.wxs "-dARTIFACTSDIR=$build_artifacts_dir" -out `"$build_artifacts_dir\\`" }
+
+    #Exec { &"$light_path" ""$build_artifacts_dir\Solutionizer.wixobj"" }
+    Exec { &"$light_path" "$build_artifacts_dir\Solutionizer.wixobj" "-dARTIFACTSDIR=$build_artifacts_dir" -out `"$build_artifacts_dir\\solutionizer.msi`" }
 }
---
+++ psake_ext.ps1
@@ -0,0 +1,107 @@
+function Expand-ZipFile {
+    param {
+       $zipPath,
+       $destination,
+       [switch] $quiet
+    }
+
+    $shellApplication = new-object -com shell.application
+    $zipPackage = $shellApplication.NameSpace($zipPath)
+    $destinationFolder = $shellApplication.NameSpace($destination) 
+
+    # CopyHere vOptions Flag # 4 - Do not display a progress dialog box. 
+    # 16 - Respond with "Yes to All" for any dialog box that is displayed. 
+    #$destinationFolder.CopyHere($zipPackage.Items(),20) 
+    $total = $zipPackage.Items().Count
+    $progress = 1;
+    foreach ($zipFile in $zipPackage.Items()) {
+        if(!$quiet) {
+            Write-Progress "Extracting $zipPath" "Extracting $($zipFile.Name)" -id 0 -percentComplete (($progress/$total)*100)        
+        }
+
+        $destinationFolder.CopyHere($zipFile,20) 
+        $progress++
+    }
+    if(!$quiet) {
+        Write-Progress "Extracted $zipPath" "Extracted $total items" -id 0
+    }
+}
+
+# Get-WebFile 3.6 (aka wget for PowerShell)
+# by Joel Bennett http://poshcode.org/417
+function Get-WebFile {
+   param( 
+      $url = (Read-Host "The URL to download"),
+      $fileName = $null,
+      [switch]$Passthru,
+      [switch]$quiet
+   )
+   
+   $req = [System.Net.HttpWebRequest]::Create($url);
+   $res = $req.GetResponse();
+ 
+   if($fileName -and !(Split-Path $fileName)) {
+      $fileName = Join-Path (Get-Location -PSProvider "FileSystem") $fileName
+   } 
+   elseif((!$Passthru -and ($fileName -eq $null)) -or (($fileName -ne $null) -and (Test-Path -PathType "Container" $fileName)))
+   {
+      [string]$fileName = ([regex]'(?i)filename=(.*)$').Match( $res.Headers["Content-Disposition"] ).Groups[1].Value
+      $fileName = $fileName.trim("\/""'")
+      if(!$fileName) {
+         $fileName = $res.ResponseUri.Segments[-1]
+         $fileName = $fileName.trim("\/")
+         if(!$fileName) { 
+            $fileName = Read-Host "Please provide a file name"
+         }
+         $fileName = $fileName.trim("\/")
+         if(!([IO.FileInfo]$fileName).Extension) {
+            $fileName = $fileName + "." + $res.ContentType.Split(";")[0].Split("/")[1]
+         }
+      }
+      $fileName = Join-Path (Get-Location -PSProvider "FileSystem") $fileName
+   }
+   if($Passthru) {
+      $encoding = [System.Text.Encoding]::GetEncoding( $res.CharacterSet )
+      [string]$output = ""
+   }
+ 
+   if($res.StatusCode -eq 200) {
+      [int]$goal = $res.ContentLength
+      $reader = $res.GetResponseStream()
+      if($fileName) {
+         $writer = new-object System.IO.FileStream $fileName, "Create"
+      }
+      [byte[]]$buffer = new-object byte[] 4096
+      [int]$total = [int]$count = 0
+      do
+      {
+         $count = $reader.Read($buffer, 0, $buffer.Length);
+         if($fileName) {
+            $writer.Write($buffer, 0, $count);
+         } 
+         if($Passthru){
+            $output += $encoding.GetString($buffer,0,$count)
+         } elseif(!$quiet) {
+            $total += $count
+            if($goal -gt 0) {
+               Write-Progress "Downloading $url" "Saving $total of $goal" -id 0 -percentComplete (($total/$goal)*100)
+            } else {
+               Write-Progress "Downloading $url" "Saving $total bytes..." -id 0
+            }
+         }
+      } while ($count -gt 0)
+      
+      $reader.Close()
+      if($fileName) {
+         $writer.Flush()
+         $writer.Close()
+      }
+      if($Passthru){
+         $output
+      }
+   }
+   $res.Close(); 
+   #if($fileName) {
+   #   ls $fileName
+   #}
+}
